8f821a0946681e1e069f844f70bed6a3241615a5
AJPS2923 <pankaj.shah2021@vitbhopal.ac.in>
2022-03-14 10:45:50 +0530

    update to previous programm

--- check_string_is_palindrome.cpp
+++ check_string_is_palindrome.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 bool helper(string n1,int start,int end)
 {
-    if(end-start==0 || end-start== 1)
+    cout<<end-start<<endl;
+    if(0>=end-start)
     {
+        cout<<"run";
         return true;
     }
     else if(n1[start]==n1[end])
     {
         return helper(n1,start+1,end-1);
     }
-    else{
+    else{ 
+        cout<<"run2";
         return false;
     }
 }
@@ -23,6 +26,7 @@ int main(){
     string name;
     cin>>name;
 
+    //cout<<name;
     cout<<check_palin(name);
     return 0;
 }
