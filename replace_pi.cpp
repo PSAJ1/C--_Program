7599ef8341f0b3b0795a599370e81eae91070edd
AJPS2923 <pankaj.shah2021@vitbhopal.ac.in>
2022-03-19 02:29:35 +0530

    replace_pi update

--- replace_pi.cpp
+++ replace_pi.cpp
@@ -3,23 +3,29 @@
 #include<vector>
 
 using  namespace std;
-string replace_pi(string &p);
+string replace_pi(string p)
 {
-    if(p.length()<=0)
+    //cout<<p.length()<<"\n";
+    //cout<<p<<endl;
+    if(p.length()<=1)
     {
-        return ;
+        return p;
     }
-    string sm=replace_pi(p+1);
+    string sm=replace_pi(p.substr(0+1,p.length()));
     if(p[0]=='p' && sm[0]=='i')
-
+    {
+        return(sm="3.14"+(sm.substr(1,sm.length())));
+    }
+    return(sm=p[0]+sm);
 }
 int main()
 {
     string info;
     //vector<string> info1;
     cin>>info;
-    cout<<sizeof(char)<<endl;
-    //cout<<info.size();
+    //string p=info+1;
+    //cout<<info[13]<<endl;
+    //cout<<info.length();
 
     cout<<replace_pi(info); 
     return 0;
