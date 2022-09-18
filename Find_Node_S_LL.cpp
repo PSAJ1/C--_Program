36567a4379b935aa52327bd932fb3fd18aae34cb
AJPS2923 <pankaj.shah2021@vitbhopal.ac.in>
2022-09-18 21:49:34 +0530

    Find Node In Single LL

---
+++ Find_Node_S_LL.cpp
@@ -0,0 +1,89 @@
+#include<iostream>
+#include "Node1.cpp"
+using namespace std;
+
+Node1* insert(int pos,int data,Node1 *head)
+{
+    if(head==NULL)
+    {
+        cout<<"Loc";
+        return head;
+    }
+    if(pos==0)
+    {
+        Node1 *ptr=new Node1(data);
+        ptr->next=head;
+        return ptr;
+    }
+    Node1 *p=insert(pos-1,data,head->next);
+    head->next=p;
+    return head;
+}
+
+Node1* takeInput(){
+    int dt;
+    cin>>dt;
+    if(dt==-1){
+        return NULL;
+    }
+    Node1 *head=NULL,*tail=NULL;
+    Node1 *newnode=new Node1(dt);
+    if(head==NULL ){
+        head=newnode;
+        tail=newnode;
+    }
+    Node1 *temp=head;
+    cin>>dt;
+    while(dt!=-1){
+        Node1 *newnode=new Node1(dt);
+        tail->next=newnode;
+        tail=newnode;
+        cin>>dt;
+    }
+    return head;
+}
+
+void print(Node1 *head)
+{
+    if(head==NULL)
+    {
+        cout<<"\nSorry no Linked List....\n";
+        return;
+    }
+    while(head!=NULL)
+    {
+        cout<<head->a<<" ";
+        head=head->next;
+    }
+    cout<<endl;
+}
+
+int Find(int data,Node1 *head)
+{
+    if(head==NULL)
+    {
+        return -1;
+    }
+    else if(data==head->a)
+    {
+        return 0;
+    }
+    int index=Find(data,head->next);
+    if(index==-1)
+    {
+        return index;
+    }
+    else{
+        return index+1;
+    }
+}
+
+int main()
+{
+    Node1 *ptr=takeInput();
+    print(ptr);
+    int data;
+    cin>>data;
+    cout<<Find(data,ptr);
+    return 0;
+}
